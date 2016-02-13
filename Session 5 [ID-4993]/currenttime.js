2f68b1a8b94224ed4b2043d0534fc75a6924260d
arvindjena008 <arvind.jena55@gmail.com>
2016-02-13 22:36:08 +0530

    update

--- Session 5 [ID-4993]/currenttime.js
+++ Session 5 [ID-4993]/currenttime.js
@@ -3,4 +3,3 @@ var currentdate = new Date();
 var datetime = currentdate.getHours() + ":" + currentdate.getMinutes();
 
 document.write("The current time is : "+datetime);
-document.write(log(datetime));
