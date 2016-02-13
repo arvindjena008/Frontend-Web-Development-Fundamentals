82d028a06a14194a5faba740f9ad15b0d1917fca
arvindjena008 <arvind.jena55@gmail.com>
2016-02-13 13:53:32 +0530

    update assignment

---
+++ Session 5 [ID-4993]/circlearea.js
@@ -0,0 +1,6 @@
+function areaofcircle(radius1,radius2,radius3)
+{
+document.write("<center>radius = "+radius1+" cm; "+"area = "+(3.14*radius1*radius1)+" cm<sup>2</sup>.<br/><br/></center>");
+document.write("<center>radius = "+radius2+" cm; "+"area = "+(3.14*radius2*radius2)+" cm<sup>2</sup>.<br/><br/></center>");
+document.write("<center>radius = "+radius3+" cm; "+"area = "+(3.14*radius3*radius3)+" cm<sup>2</sup>.<br/><br/></center>");
+}
---
+++ Session 5 [ID-4993]/remnum.js
@@ -0,0 +1,7 @@
+function remofnum(num1,num2)
+{
+	var x=num1;
+	var y=num2;
+	var r= x % y;
+document.write("The Remainder of "+x+" & "+y+" is: "+r);
+}
